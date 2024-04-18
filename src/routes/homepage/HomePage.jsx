69e112d6726b7bba04241fbcb4c4786db7b43be8
Robert Gleason <l2ob122@gmail.com>
2024-04-17 20:57:17 -0700

    Add homepage route and component, styling image section

---
+++ src/routes/homepage/HomePage.jsx
@@ -0,0 +1,13 @@
+import './homePage.scss';
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <div className='textContainer'>text</div>
+      <div className='imageContainer'>
+        <img src='/bg.png' alt='' />
+      </div>
+    </div>
+  );
+};
+export default HomePage;
